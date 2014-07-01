05e3b21ad1ddced3be5c9354612f0069cee9093f
Markus Stockbauer <m.stockbauer@techdivision.com>
2014-07-02 00:04:51 +0200

    added author

--- examples/magevm.php
+++ examples/magevm.php
@@ -13,6 +13,7 @@
  * @category  Webserver
  * @package   TechDivision_MageModule
  * @author    Johann Zelger <jz@techdivision.com>
+ * @author    Markus Stockbauer <ms@techdivision.com>
  * @copyright 2014 TechDivision GmbH <info@techdivision.com>
  * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  * @link      https://github.com/techdivision/TechDivision_MageModule
