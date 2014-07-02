9b4880b264585462327b60519f594e354ca608b4
Markus Stockbauer <m.stockbauer@techdivision.com>
2014-07-02 19:31:23 +0200

    Added author comment

--- examples/magevm.php
+++ examples/magevm.php
@@ -31,6 +31,7 @@ use \TechDivision\Server\Sockets\StreamSocket;
  * @category  Webserver
  * @package   TechDivision_MageModule
  * @author    Johann Zelger <jz@techdivision.com>
+ * @author    Markus Stockbauer <ms@techdivision.com>
  * @copyright 2014 TechDivision GmbH <info@techdivision.com>
  * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  * @link      https://github.com/techdivision/TechDivision_MageModule
